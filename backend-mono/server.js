cac96b07583d2f9f9d66955a35929b4301fe32d0
Yassine Eluharani <eluharani.yassine@gmail.com>
2024-09-15 21:32:48 +0100

    payload signature webhook fix

--- backend-mono/server.js
+++ backend-mono/server.js
@@ -11,6 +11,9 @@ const booking_routes = require("./routes/booking.routes");
 const app = express()
 
 app.use(cors());
+
+app.use("/api/user", user_routes);
+
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
@@ -19,7 +22,6 @@ app.get('/', async (req, res) => {
   res.json({ message: 'backend monolith' });
 })
 
-app.use("/api/user", user_routes);
 app.use("/api/listings", listing_routes);
 app.use("/api/booking", booking_routes);
 
